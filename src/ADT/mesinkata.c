16c1d95c1ea154c596ed87d1eb28ceb01d1c7541
GYANMAIZIKO <110335937+GYANMAIZIKO@users.noreply.github.com>
2022-11-04 22:39:07 +0700

    Update RNG.c

--- src/ADT/mesinkata.c
+++ src/ADT/mesinkata.c
@@ -5,58 +5,70 @@ boolean EndWord;
 Word currentWord;
 Word CCommand;
 
-void IgnoreBlanks() {
-/* Mengabaikan Satu atau beberapa BLANK 
-   I.S  : currentChar sembarang
-   F.S  : currentChar != BLANK atau currentChar != ENTER */
-    while ((currentChar == BLANK) || (currentChar != ENTER)) {
+void IgnoreBlanks()
+{
+    /* Mengabaikan Satu atau beberapa BLANK
+       I.S  : currentChar sembarang
+       F.S  : currentChar != BLANK atau currentChar != ENTER */
+    while ((currentChar == BLANK) || (currentChar != ENTER))
+    {
         ADV();
     }
 }
 
-void STARTWORD(char* filename) {
-/* I.S. : currentChar sembarang 
-   F.S. : EndWord = true, dan currentChar = MARK; 
-          atau EndWord = false, currentWord adalah kata yang sudah diakuisisi,
-          currentChar karakter pertama sesudah karakter terakhir kata */
-          
+void STARTWORD(char *filename)
+{
+    /* I.S. : currentChar sembarang
+       F.S. : EndWord = true, dan currentChar = MARK;
+              atau EndWord = false, currentWord adalah kata yang sudah diakuisisi,
+              currentChar karakter pertama sesudah karakter terakhir kata */
+
     START(filename);
     IgnoreBlanks();
-    if (currentChar == MARK) {
+    if (currentChar == MARK)
+    {
         EndWord = true;
-    } else {
+    }
+    else
+    {
         EndWord = false;
         ADVWORD();
         CopyWord();
     }
 }
 
-void ADVWORD() {
-/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
-   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
-          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
-          Jika currentChar = MARK, EndWord = true.		  
-   Proses : Akuisisi kata menggunakan procedure CopyWord */
-   
+void ADVWORD()
+{
+    /* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi
+       F.S. : currentWord adalah kata terakhir yang sudah diakuisisi,
+              currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
+              Jika currentChar = MARK, EndWord = true.
+       Proses : Akuisisi kata menggunakan procedure CopyWord */
+
     IgnoreBlanks();
-    if (currentChar == MARK && !EndWord) {
+    if (currentChar == MARK && !EndWord)
+    {
         EndWord = true;
-    } else {
+    }
+    else
+    {
         CopyWord();
         IgnoreBlanks();
     }
 }
 
-void CopyWord() {
-/* Mengakuisisi kata, menyimpan dalam currentWord
-   I.S. : currentChar adalah karakter pertama dari kata
-   F.S. : currentWord berisi kata yang sudah diakuisisi; 
-          currentChar = BLANK atau currentChar = MARK; 
-          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+void CopyWord()
+{
+    /* Mengakuisisi kata, menyimpan dalam currentWord
+       I.S. : currentChar adalah karakter pertama dari kata
+       F.S. : currentWord berisi kata yang sudah diakuisisi;
+              currentChar = BLANK atau currentChar = MARK;
+              currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
+              Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
 
     int i = 0;
-    while ((currentChar != MARK) && (currentChar != BLANK) && (i < NMax)) {
+    while ((currentChar != MARK) && (currentChar != BLANK) && (i < NMax))
+    {
         currentWord.TabWord[i] = currentChar;
         ADV();
         i++;
@@ -64,57 +76,69 @@ void CopyWord() {
     currentWord.Length = i;
 }
 
-void IgnoreDot(){
-/* Mengabaikan satu atau beberapa BLANK dan MARK
-   I.S. : currentChar sembarang 
-   F.S. : currentChar ≠ BLANK atau currentChar = ENTER */
-   while ((currentChar == BLANK) && (currentChar == '.')){
+void IgnoreDot()
+{
+    /* Mengabaikan satu atau beberapa BLANK dan MARK
+       I.S. : currentChar sembarang
+       F.S. : currentChar ≠ BLANK atau currentChar = ENTER */
+    while ((currentChar == BLANK) && (currentChar == '.'))
+    {
         ADVC();
-   }
+    }
 }
 
-void STARTCOMMAND(){
-/* I.S. : currentChar sembarang 
-   F.S. : EndWord = true, dan currentChar = ENTER; 
-          atau EndWord = false, CCommand adalah kata yang sudah diakuisisi,
-          currentChar karakter pertama sesudah karakter terakhir kata */
-   COMMAND();
-   IgnoreDot();
-   if(currentChar == ENTER){
+void STARTCOMMAND()
+{
+    /* I.S. : currentChar sembarang
+       F.S. : EndWord = true, dan currentChar = ENTER;
+              atau EndWord = false, CCommand adalah kata yang sudah diakuisisi,
+              currentChar karakter pertama sesudah karakter terakhir kata */
+    COMMAND();
+    IgnoreDot();
+    if (currentChar == ENTER)
+    {
         EndWord = true;
-   } else {
+    }
+    else
+    {
         EndWord = false;
         ADVCOMMAND();
-   }
+    }
 }
 
-void ADVCOMMAND(){
-/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
-   F.S. : CComand adalah kata terakhir yang sudah diakuisisi, 
-          currentChar adalah karakter pertama dari kata berikutnya, mungkin ENTER
-          Jika currentChar = ENTER, EndKata = true.		  
-   Proses : Akuisisi kata menggunakan procedure SalinCommand */
-   IgnoreDot();
-   if(currentChar == ENTER && !EndKata){
+void ADVCOMMAND()
+{
+    /* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi
+       F.S. : CComand adalah kata terakhir yang sudah diakuisisi,
+              currentChar adalah karakter pertama dari kata berikutnya, mungkin ENTER
+              Jika currentChar = ENTER, EndWord = true.
+       Proses : Akuisisi kata menggunakan procedure SalinCommand */
+    IgnoreDot();
+    if (currentChar == ENTER && !EndWord)
+    {
         EndWord = true;
-   } else {
+    }
+    else
+    {
         CopyCommand();
         IgnoreDot();
-   }
+    }
 }
 
-void CopyCommand(){
-/* Mengakuisisi kata, menyimpan dalam CComand
-   I.S. : currentChar adalah karakter pertama dari kata
-   F.S. : CComand berisi kata yang sudah diakuisisi; 
-          currentChar = BLANK atau currentChar = ENTER; 
-          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
-   int j = 0;
-   while((currentChar != BLANK) && (currentChar != ENTER) && j != NMax){
+void CopyCommand()
+{
+    /* Mengakuisisi kata, menyimpan dalam CComand
+       I.S. : currentChar adalah karakter pertama dari kata
+       F.S. : CComand berisi kata yang sudah diakuisisi;
+              currentChar = BLANK atau currentChar = ENTER;
+              currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
+              Jika panjang kata melebihi NMax, maka sisa kata "dipotong" */
+    int j = 0;
+    while ((currentChar != BLANK) && (currentChar != ENTER) && j != NMax)
+    {
         CCommand.TabWord[j] = currentChar;
         ADVC();
         j++;
-   }
-   CCommand.Length = (j < NMax) ? j : NMax;
+    }
+    CCommand.Length = (j < NMax) ? j : NMax;
 }
--- src/Game/RNG/RNG.c
+++ src/Game/RNG/RNG.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int randomDadu()
+{
+    srand((unsigned)time(0));
+    return (rand() % 100) + 1;
+}
+
+int main()
+{
+    int count = randomDadu();
+    int n;
+    printf("Tebakan: ");
+    scanf("%d", &n);
+    while (n != count)
+    {
+        if (n > count)
+        {
+            printf("Lebih Kecil\n");
+            printf("Tebakan: ");
+            scanf("%d", &n);
+        }
+        else if (n < count)
+        {
+            printf("Lebih Besar\n");
+            printf("Tebakan: ");
+            scanf("%d", &n);
+
+        }
+    }
+    printf("Ya, X adalah %d\n", n);
+    return 0;
+}
