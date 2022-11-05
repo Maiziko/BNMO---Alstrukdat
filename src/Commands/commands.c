fa85ccc8910e7fb1cfc2596e31fd199cb53de110
ZevonikRF <rifky.fachrizain@gmail.com>
2022-11-05 09:44:18 +0700

    added command "start" (belum fix)

--- src/Commands/commands.c
+++ src/Commands/commands.c
@@ -1,1 +1,19 @@
 // Isi-isi commands ada disini yah
+
+// START
+#include "mesinkata.h"
+
+void start()
+{
+    STARTWORD();
+    if (&currentWord == "START")
+    {
+        printf("File konfigurasi sistem berhasil dibaca. BNMO berhasil dijalankan.");
+        // Fungsi2 ntar
+    }
+    else
+    {
+        printf("Input tidak valid.");
+        
+    }
+}
