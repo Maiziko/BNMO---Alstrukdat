c10b320be43d9b1553a49e7a8f20945d719a2997
nurshafaqn <emailnyaucha@gmail.com>
2022-11-11 09:42:50 +0700

    update diner dash

--- src/Game/Dinner Dash/tempCodeRunnerFile.c
+++ src/Game/Dinner Dash/tempCodeRunnerFile.c
@@ -1,1 +1,6 @@
-de
+               // for(int i = 0; i < length(saji); i++) {
+                //     if (saji.buffer[i].urut == apa) {
+                //         dequeue(&pesan, &(pesan.buffer[i]));
+                //         dequeue(&saji, &(pesan.buffer[i]));                      
+                //     }
+                // } 
