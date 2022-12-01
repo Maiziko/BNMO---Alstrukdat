23c0e9132c96ee1da6a0aa230cc2b829608a0381
GYANMAIZIKO <110335937+GYANMAIZIKO@users.noreply.github.com>
2022-12-01 11:45:59 +0700

    Ngapus yang ga perlu

--- src/ADT/SaveLoad/load.c
+++ src/ADT/SaveLoad/load.c
@@ -6,10 +6,6 @@
 ArrayDin Load(Word kata)
 {
     ArrayDin ffile = MakeArrayDin();
-    char gm[100];
-    char gm1[100];
-    char gm2[100];
-    char m[100];
     char filetext[100];
     int i = 0;
     int j = 0;
@@ -22,57 +18,29 @@ ArrayDin Load(Word kata)
 
     for (j = 0; j < CCommand.Length; j++)
     {
-        gm[j] = CCommand.TabWord[j];
-    }
-    gm[j] = '\0';
-
-    for (j = 0; j < CCommand.Length; j++)
-    {
-        m[j] = CCommand.TabWord[j + 5];
-    }
-
-    for (j = 0; j < CCommand.Length; j++)
-    {
-        gm1[j] = gm[j + 5];
-    }
-    gm1[j] = '\0';
-
-    for (j = 0; j < CCommand.Length; j++)
-    {
         filetext[j + 5] = CCommand.TabWord[j + 5];
     }
     filetext[j] = '\0';
 
-    for (j = 0; j < 4; j++)
+    if (KSTART(filetext) == 1)
     {
-        gm2[j] = CCommand.TabWord[j];
-    }
-    gm2[j] = '\0';
-
-    if (IsKataSama(toKata(gm2), toKata("LOAD")))
-    {
-        if (IsKataSama(toKata(gm1), toKata(m)))
+        STARTGAME(filetext);
+        while (!EndWord)
         {
-            if (KSTART(filetext) == 1)
+            for (int j = 0; j < currentWord.Length; j++)
             {
-                STARTGAME(filetext);
-                while (!EndWord)
-                {
-                    for (int j = 0; j < currentWord.Length; j++)
-                    {
-                        ffile.A[i].TabWord[j] = currentWord.TabWord[j];
-                    }
-                    ffile.A[i].Length = currentWord.Length;
-                    ADVWORD();
-                    i++;
-                }
-                ffile.Neff = i;
+                ffile.A[i].TabWord[j] = currentWord.TabWord[j];
             }
-            else
-            {
-                ffile.Neff = 0;
-            }
+            ffile.A[i].Length = currentWord.Length;
+            ADVWORD();
+            i++;
         }
+        ffile.Neff = i;
     }
+    else
+    {
+        ffile.Neff = 0;
+    }
+
     return (ffile);
 }
